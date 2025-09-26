d63b9de0bb471df06b71d3c3c99470f312581611
gitjip <2672259086@qq.com>
2025-09-26 14:42:34 +0800

    Untrack old files.

---
+++ client/usercenterwidget.h
@@ -0,0 +1,37 @@
+#ifndef USERCENTERWIDGET_H
+#define USERCENTERWIDGET_H
+
+#include "tcpclient.h"
+#include <QWidget>
+
+namespace Ui {
+class UserCenterWidget;
+}
+
+class UserCenterWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit UserCenterWidget(QWidget *parent = nullptr);
+    ~UserCenterWidget();
+    void setClient(My::TcpClient *client);
+    void setUserId(int userId);
+    void refresh();
+
+signals:
+    void readySetUserId(int userId);
+
+private slots:
+    void on_pushButtonLogin_clicked();
+    void on_pushButtonRegister_clicked();
+    void on_pushButtonLogout_clicked();
+    void on_pushButtonUnregister_clicked();
+
+private:
+    Ui::UserCenterWidget *ui;
+    My::TcpClient *client;
+    int userId;
+};
+
+#endif // USERCENTERWIDGET_H
